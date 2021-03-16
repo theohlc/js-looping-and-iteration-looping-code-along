eb35b5901a7337de209a8e391d0bca71cd860c5c
theo <teddyhoover@gmail.com>
2021-03-16 13:25:04 -0500

    commit tree f878ad6a1f985ff01cff0c3a4515a554d73d788a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(guestNames, eventName) {
+    let a = []
+    for (let i = 0; i < guestNames.length; i++) {
+        a.push( "Thank you, " + guestNames[i] + ", for the wonderful " + eventName + " gift!" );
+        
+    }
+    return a
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number = number - 1;
+    }
+}
